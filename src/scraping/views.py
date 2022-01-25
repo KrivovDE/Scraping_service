85d5794972b52f29954cf45fdf1de95b1b826218
KrivovDE <55reigen55@mail.ru>
2022-01-25 14:51:17 +0300

    Display changed. Added pagination.

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 from .forms import FindForm
 from .models import Vacancy
@@ -6,16 +8,23 @@ from .models import Vacancy
 
 def home_view(request):
     form = FindForm()
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    gs = []
-
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
             _filter['city__slug'] = city
         if language:
             _filter['language__slug'] = language
-
-        gs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': gs, 'form': form})
+        qs = Vacancy.objects.filter(**_filter)
+        paginator = Paginator(qs, 10)  # Show 10 contacts per page.
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+    return render(request, 'scraping/list.html', context)
